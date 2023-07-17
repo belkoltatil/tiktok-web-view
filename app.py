c996090f411553c26c1b9435a86ff1f0d399eb3f
belkoltatil <124864350+belkoltatil@users.noreply.github.com>
2023-07-17 14:51:56 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,48 @@
-from flask import Flask, render_template
+import asyncio
 import re
-import requests
+import aiohttp
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+async def process_url(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.text()
+            matches = re.findall(r'<pre style="word-wrap: break-word; white-space: pre-wrap;">(.*?)<\/pre>', data)
+            if matches:
+                return matches[0]
+            return None
+
 @app.route('/')
 def index():
-    url = 'https://hello-world-restless-lake-f4e4.devdesk.workers.dev'
+    base_url = 'https://hello-world-restless-lake-f4e4.devdesk.workers.dev'
     regex = r'\b\w+\:\/\/[^\s"]+'
-
     completed_links = []
 
+    loop = asyncio.get_event_loop()
+    tasks = []
+
     while True:
-        response = requests.get(url)
+        response = requests.get(base_url)
         matches = re.findall(regex, response.text)
-        
+
         if not matches:
             break
 
         for match in matches:
             completed_link = 'https://hello-world-dawn-sunset-aa71.devdesk.workers.dev/?url=' + match
             completed_links.append(completed_link)
-            response = requests.get(completed_link)
-            value = re.findall(r'<pre style="word-wrap: break-word; white-space: pre-wrap;">(.*?)<\/pre>', response.text)
-            
-            if value:
-                completed_links.append(value[0])
-            
-            url = completed_link
+            tasks.append(process_url(completed_link))
+
+        results = loop.run_until_complete(asyncio.gather(*tasks))
+        tasks = []
+
+        for result in results:
+            if result:
+                completed_links.append(result)
+
+        base_url = completed_links[-1] if completed_links else None
 
     return render_template('index.html', links=completed_links)
 
