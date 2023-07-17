7373268583c080397aa85d97a528ffb0080f4290
belkoltatil <124864350+belkoltatil@users.noreply.github.com>
2023-07-17 14:13:03 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,27 @@
+from flask import Flask, render_template
 import requests
 from bs4 import BeautifulSoup
-from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Retrieve href attribute values from the initial link
-    initial_link = 'https://hello-world-restless-lake-f4e4.devdesk.workers.dev'
-    response = requests.get(initial_link)
+    url = 'https://hello-world-restless-lake-f4e4.devdesk.workers.dev'
+    response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
-    href_values = [a['href'] for a in soup.find_all('a', href=True)]
+    hrefs = [a['href'] for a in soup.find_all('a', href=True)]
 
-    # Process each href value
-    video_sources = []
-    for href in href_values:
-        completed_link = f'https://hello-world-dawn-sunset-aa71.devdesk.workers.dev/?url={href}'
-        completed_response = requests.get(completed_link)
+    videos = []
+    for href in hrefs:
+        completed_url = 'https://hello-world-dawn-sunset-aa71.devdesk.workers.dev/?url=' + href
+        completed_response = requests.get(completed_url)
         completed_soup = BeautifulSoup(completed_response.text, 'html.parser')
-        pre_tag = completed_soup.find('pre')
+        pre_text = completed_soup.find('pre').get_text()
 
-        if pre_tag:
-            text_inside_pre = pre_tag.text.strip()
-            video_sources.append(text_inside_pre)
+        video_tag = f'<video src="{pre_text}"></video>'
+        videos.append(video_tag)
 
-    return render_template('index.html', video_sources=video_sources)
+    return render_template('index.html', videos=videos)
 
 if __name__ == '__main__':
     app.run()
