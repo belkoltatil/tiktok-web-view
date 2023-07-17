4d7cba02cff1c626982c5f8fa0cf78b9daad1f44
belkoltatil <124864350+belkoltatil@users.noreply.github.com>
2023-07-17 14:42:56 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,35 @@
 from flask import Flask, render_template
+import re
 import requests
-from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     url = 'https://hello-world-restless-lake-f4e4.devdesk.workers.dev'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    hrefs = [a['href'] for a in soup.find_all('a', href=True)]
+    regex = r'\b\w+\:\/\/[^\s"]+'
 
-    videos = []
-    for href in hrefs:
-        completed_url = 'https://hello-world-dawn-sunset-aa71.devdesk.workers.dev/?url=' + href
-        completed_response = requests.get(completed_url)
-        completed_soup = BeautifulSoup(completed_response.text, 'html.parser')
-        pre_tag = completed_soup.find('pre')
-        if pre_tag:
-            pre_text = pre_tag.get_text().strip()
-            video_content = requests.get(pre_text).content
+    completed_links = []
 
-            video_filename = f'video_{len(videos)}.mp4'
-            with open(video_filename, 'wb') as f:
-                f.write(video_content)
+    while True:
+        response = requests.get(url)
+        matches = re.findall(regex, response.text)
+        
+        if not matches:
+            break
 
-            video_tag = f'<video controls><source src="{video_filename}" type="video/mp4"></video>'
-            videos.append(video_tag)
+        for match in matches:
+            completed_link = 'https://hello-world-dawn-sunset-aa71.devdesk.workers.dev/?url=' + match
+            completed_links.append(completed_link)
+            response = requests.get(completed_link)
+            value = re.findall(r'<pre style="word-wrap: break-word; white-space: pre-wrap;">(.*?)<\/pre>', response.text)
+            
+            if value:
+                completed_links.append(value[0])
+            
+            url = completed_link
 
-    return render_template('index.html', videos=videos)
+    return render_template('index.html', links=completed_links)
 
 if __name__ == '__main__':
     app.run()
